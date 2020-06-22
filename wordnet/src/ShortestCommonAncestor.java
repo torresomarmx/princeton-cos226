10a1d0d3768731534070a6a822625c59b6374ec1
Omar <torresomarmx@gmail.com>
2020-06-22 08:16:16 -0400

    add test for sca

--- wordnet/src/ShortestCommonAncestor.java
+++ wordnet/src/ShortestCommonAncestor.java
@@ -1,4 +1,6 @@
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -14,7 +16,14 @@ public class ShortestCommonAncestor {
 
     // length of shortest ancestral path between v and w
     public int length(int v, int w) {
+        this.validateVertex(v);
+        this.validateVertex(w);
         return this.getShortestCommonAncestor(Collections.singletonList(v), Collections.singletonList(w))[1];
+    }
+
+    private void validateVertex(int v) {
+        if (v < 0 || v >= this.digraph.V())
+            throw new IllegalArgumentException("Vertex " + v + " is out of bounds");
     }
 
     private void getVerticesInPathToRoot(int v, int currentPathLength, HashMap<Integer, Integer> vertexInPathToLengthOfPath) {
@@ -46,22 +55,49 @@ public class ShortestCommonAncestor {
         return new int[]{shortestCommonAncestor, lengthOfShortestAncestralPath};
     }
 
+    private void validateVertexSet(Iterable<Integer> subsetA) {
+        if (subsetA == null) throw new IllegalArgumentException("Set of vertices is null");
+        int count = 0;
+        for (Integer vertex : subsetA) {
+            count += 1;
+            this.validateVertex(vertex);
+        }
+        if (count == 0) throw new IllegalArgumentException("Set of vertices is empty");
+    }
+
     // a shortest common ancestor of vertices v and w
     public int ancestor(int v, int w) {
+        this.validateVertex(v);
+        this.validateVertex(w);
         return this.getShortestCommonAncestor(Collections.singletonList(v), Collections.singletonList(w))[0];
     }
 
     // length of shortest ancestral path of vertex subsets A and B
     public int lengthSubset(Iterable<Integer> subsetA, Iterable<Integer> subsetB) {
+        this.validateVertexSet(subsetA);
+        this.validateVertexSet(subsetB);
         return this.getShortestCommonAncestor(subsetA, subsetB)[1];
     }
 
     // a shortest common ancestor of vertex subsets A and B
     public int ancestorSubset(Iterable<Integer> subsetA, Iterable<Integer> subsetB) {
+        this.validateVertexSet(subsetA);
+        this.validateVertexSet(subsetB);
         return this.getShortestCommonAncestor(subsetA, subsetB)[0];
     }
 
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        ShortestCommonAncestor sca = new ShortestCommonAncestor(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length   = sca.length(v, w);
+            int ancestor = sca.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
+    }
 
 }

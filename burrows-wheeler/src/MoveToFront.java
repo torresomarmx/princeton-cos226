796390d643ee708eab41f051b92cc4a8da1bf7c2
Omar <torresomarmx@gmail.com>
2020-07-19 18:39:59 -0400

    write move to front encode function

---
+++ burrows-wheeler/src/MoveToFront.java
@@ -0,0 +1,33 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+import java.util.LinkedList;
+
+public class MoveToFront {
+
+    // apply move-to-front encoding, reading from stdin and writing to stdout
+    public static void encode() {
+        LinkedList<Character> alphabet = new LinkedList<>();
+        for (int i = 0; i < 255; i++)
+            alphabet.add((char) i);
+
+        while (!BinaryStdIn.isEmpty()) {
+            char inChar = BinaryStdIn.readChar();
+            int index = alphabet.indexOf(inChar);
+            BinaryStdOut.write(index, 8);
+            alphabet.remove(index);
+            alphabet.addFirst(inChar);
+        }
+        BinaryStdOut.close();
+    }
+//
+//    // apply move-to-front decoding, reading from stdin and writing to stdout
+//    public static void decode()
+//
+    // if args[0] is "-", apply move-to-front encoding
+    // if args[0] is "+", apply move-to-front decoding
+    public static void main(String[] args) {
+        MoveToFront.encode();
+    }
+
+}
